d08acca17351638a9ca6a159c128694fad38173b
Rayssa farias <rayssa.farias1606@gmail.com>
2024-08-31 12:03:10 -0300

    Trabalhando com funções

---
+++ AULA 06/js/biblioteca.js
@@ -0,0 +1,27 @@
+function somaArray(array){
+let resul = 0
+for(let i = 0 ; i< array.length;i++)  {
+
+    resul += array[i]
+}
+
+return resul
+
+
+}
+
+
+
+
+function mediaArray(array){
+    let resul = 0
+    for(let i = 0 ; i< array.length;i++)  {
+    
+        resul += array[i]
+    }
+    
+    return resul/array.length
+    
+    
+    }
+    
